c6b1a5e81853cade2b946443164ca751b2204d9e
bryansan1226 <70733299+bryansan1226@users.noreply.github.com>
2022-09-15 15:19:25 -0400

    Using bootstrap for layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import portrait from "./assets/portrait.png";
 import EmailIcon from "@mui/icons-material/Email";
@@ -9,6 +10,12 @@ import SummarizeIcon from "@mui/icons-material/Summarize";
 import PdfViewer from "./components/PdfViewer";
 import Backdrop from "./components/Backdrop";
 import ProjectContainer from "./components/ProjectContainer";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function App(props) {
   const [pdfIsOpen, setPdfIsOpen] = useState(false);
@@ -20,53 +27,63 @@ function App(props) {
     setPdfIsOpen(false);
   }
   return (
-    <div className="homepage">
-      {pdfIsOpen && <PdfViewer />}
-      {pdfIsOpen && <Backdrop onClick={closePdfHandler} />}
-      <div className="sidebar">
-        <div className="fullName" maxWidth="sm">
-          Bryan Sanchez
-        </div>
-        <div>
-          <img src={portrait} className="Portrait" />
-        </div>
-        <div className="contactInfo">
-          <p>
-            <EmailIcon />
-            <a href="mailto: bryan.sanchez1226@gmail.com">
-              bryan.sanchez1226@gmail.com
-            </a>
-          </p>
-          <p>
-            <PhoneIphoneIcon />
-            (347)-785-7839
-          </p>
-          <p>
-            <LinkedInIcon />
-            <a href="https://www.linkedin.com/in/bryansanchez1226/">
-              bryansanchez1226
-            </a>
-          </p>
-          <p>
-            <GitHubIcon />
-            <a href="https://github.com/bryansan1226">bryansan1226</a>
-          </p>
-          {/* <p>
+    <Container fluid>
+      <div className="homepage">
+        {pdfIsOpen && <PdfViewer />}
+        {pdfIsOpen && <Backdrop onClick={closePdfHandler} />}
+        <Row>
+          <Col md={3}>
+            <div className="sidebar">
+              <div className="fullName" maxWidth="sm">
+                Bryan Sanchez
+              </div>
+              <div>
+                <Image fluid src={portrait} className="Portrait" />
+              </div>
+              <Card class="opacity-100">
+                <div className="contactInfo">
+                  <p>
+                    <EmailIcon />
+                    <a href="mailto: bryan.sanchez1226@gmail.com">
+                      bryan.sanchez1226@gmail.com
+                    </a>
+                  </p>
+                  <p>
+                    <PhoneIphoneIcon />
+                    (347)-785-7839
+                  </p>
+                  <p>
+                    <LinkedInIcon />
+                    <a href="https://www.linkedin.com/in/bryansanchez1226/">
+                      bryansanchez1226
+                    </a>
+                  </p>
+                  <p>
+                    <GitHubIcon />
+                    <a href="https://github.com/bryansan1226">bryansan1226</a>
+                  </p>
+                  {/* <p>
             <button className="openResume" onClick={openPdfHandler}>
               <SummarizeIcon style={{ color: "black" }} />
               Resume
             </button>
   </p>*/}
-        </div>
-      </div>
-      <div className="content">
-        <div>Coding Experience</div>
+                </div>
+              </Card>
+            </div>
+          </Col>
+          <Col>
+            <div className="content">
+              <div>Coding Experience</div>
 
-        <ProjectContainer />
-        <ProjectContainer />
-        <ProjectContainer />
+              <ProjectContainer />
+              <ProjectContainer />
+              <ProjectContainer />
+            </div>
+          </Col>
+        </Row>
       </div>
-    </div>
+    </Container>
   );
 }
 
